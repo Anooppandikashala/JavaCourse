c83958756876f810a8ab876d0483fa405e59d9bf
anooppandikashala <anooppandikashala@gmail.com>
2022-07-26 23:31:56 +0530

    Added keywords

---
+++ VariablesAndDataTypes/KeywordAndIdentifiers.java
@@ -0,0 +1,33 @@
+
+//Keywords
+
+// abstract	assert	boolean	break	byte
+// case	catch	char	class	const
+
+//Identifiers
+
+// Rules for Identifiers
+
+//Identifiers cannot be a keyword.
+//Identifiers are case-sensitive.
+//It can have a sequence of letters and digits. 
+//However, it must begin with a letter, $ or _. The first letter of an identifier cannot be a digit.
+//It's a convention to start an identifier with a letter rather and $ or _.
+//Whitespaces are not allowed.
+//Similarly, you cannot use symbols such as @, #, and so on.
+
+// age
+// age32
+// 32AGE
+// $GHSDH
+// _ABC
+// _abc
+// abc Abc ABC aBc
+
+class KeywordAndIdentifiers
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Keywords And Identifiers!");
+    }
+}
