17cb67e602dfee94c6b33456172bccdc8aaa4020
anooppandikashala <anooppandikashala@gmail.com>
2022-07-26 22:50:29 +0530

    added structure

---
+++ Structure/FirstProgram.java
@@ -0,0 +1,20 @@
+// this is single line comment
+
+/*
+ This is multi-line comment
+This program is for printing Hello World
+dkflsjfjksdjvjfdk
+ldsafjkjdfvjsdfsdfs
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+class FirstProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
