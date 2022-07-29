ccb2601cea112bcfbb1cb48e61f9ac7e7c64ac4c
anooppandikashala <anooppandikashala@gmail.com>
2022-07-30 00:02:37 +0530

    added Variables And Literals

---
+++ VariablesAndDataTypes/VariablesAndLiterals.java
@@ -0,0 +1,48 @@
+/* 
+Variables and Literals
+
+Variables : location in memory
+
+literals : value that can be used directly in code
+
+*/
+
+
+
+class VariablesAndLiterals {
+    public static void main(String[] args) 
+    {
+
+        // Variables creation
+        // decleration
+        // syntax
+        // data_type variable_name;
+
+
+
+        // decleration + definition
+        // syntax
+        // data_type variable_name = value;
+
+
+        // data-types
+        /*
+         * 1. int ---- 10,100, 200, 258000
+         * 2. float,double  ----- 10.25, 58.6699
+         * 3. boolean --- true, false
+         * 4. char ---- 'a','b','t',...etc 
+         * 5. String ---- "anuraj", "seetha"
+         * 
+         */
+
+
+         int age = 18;
+         double salary = 45000.5;
+         String name = "Anuraj";
+         char grade = 'A';
+         boolean isIndian = true;
+
+    }
+}
+
+
