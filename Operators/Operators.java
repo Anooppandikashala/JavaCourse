975f33b3b6cd5a5c055e3737181809d6dbf2498e
anooppandikashala <anooppandikashala@gmail.com>
2022-07-30 23:15:41 +0530

    added Operators

---
+++ Operators/Operators.java
@@ -0,0 +1,131 @@
+/*
+    
+Operators in Java:
+
+un-comment the code
+
+*/
+
+
+class Operators {
+    public static void main(String[] args) {
+
+        //  1 . Arithmetic
+        //  + , - , * , / , %
+
+            // int mark1 = 100;
+            // int mark2 = 56;
+
+            // int result1 = mark1 + mark2;
+            // int result2 = mark1- mark2;
+            // int result3 = mark1 * mark2;
+            // int result4 = mark1 / mark2;
+            // int result5 = mark1 % mark2;
+
+            // System.out.println(result1);
+            // System.out.println(result2);
+            // System.out.println(result3);
+            // System.out.println(result4);
+            // System.out.println(result5);
+
+
+        //  2 . Assignment
+        //  = 
+
+            // int x = 100;
+            // float y = 453.656f;
+            // char c = 'a';
+
+        // 3 . Relational
+
+            // int x = 100;
+            // int y = 522;
+
+            // boolean isGreater = x > y;
+            // System.out.println(isGreater);
+
+            // boolean isLess = x < y;
+            // System.out.println(isLess);
+
+            // boolean isLessOrEqual = x <= y;
+            // System.out.println(isLessOrEqual);
+
+            // boolean isGreaterOrEqual = x >= y;
+            // System.out.println(isGreaterOrEqual);
+
+            // boolean isEqual = x == y;
+            // System.out.println(isEqual);
+
+            // boolean notEqual = x != y;
+            // System.out.println(notEqual);
+
+        // 4 . Logical
+        //  AND --- &&
+        // OR --- ||
+        // NOT --- !
+
+            // int x =10;
+            // int y = 20;
+            // int p = 100;
+            // int q = 200;
+
+        // // AND operator
+
+            // boolean result1 = (x < y) && (p > q);
+            // System.out.println(result1);
+
+        // // OR operator
+
+            // boolean result2 = (x < y) || (p > q);
+            // System.out.println(result2);
+
+        // // NOT operator
+
+            // boolean result3 = !(p > q);
+            // System.out.println(result3);
+
+
+        //  5 . Unary Operators
+        //  +, -, ++, -- , !
+
+            // int x = -52;
+            // System.out.println(x);
+
+            // int p = 100;
+            // System.out.println(++p);
+            // System.out.println(--p);
+            // System.out.println(--p);
+
+
+            // boolean b = true;
+            // System.out.println(!b);
+
+
+        // 6 . Bitwise
+        //  - , << , >> , >>> , &, |
+
+        // 7 . instanceof
+
+            // String str = "trycle";
+
+            // boolean result = str instanceof String;
+
+            // System.out.println(result);
+            
+
+        // 8. Ternary
+
+        //  if a condition true --- return 100
+        // else return 200
+
+        //  syntax
+        //  variable  = Expression ? expression1 : expression2
+            
+            // int x = 10;
+
+            // int result = x > 500 ? 2000 : 9000;
+            // System.out.println(result);
+
+    }
+}
+
